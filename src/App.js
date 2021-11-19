dc81f1805cf57ab2e00dc91ea30591714a953c4c
Jordan Johnson <jordaninjohnson@gmail.com>
2021-11-18 22:33:54 -0700

    added j logo and changed colors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './j.jpeg';
 import './App.css';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
