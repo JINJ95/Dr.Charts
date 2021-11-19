2c6def2a6a3f9ac9b81ff065ad4f4e7fd557d42b
Jordan Johnson <jordaninjohnson@gmail.com>
2021-11-18 23:04:03 -0700

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Medical Charting made easy.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get Started
         </a>
       </header>
     </div>
